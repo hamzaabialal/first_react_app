5ece154d5c59ca0d43150feaae984b977c053d41
Hamza Bilal <faizan@axya.co>
2024-08-09 16:58:29 +0500

    Added first code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Offcanvas, Button, Form, Nav, NavDropdown } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="light" expand={false} fixed="top">
+      <Navbar.Brand href="/">Offcanvas navbar</Navbar.Brand>
+      <Navbar.Toggle aria-controls="offcanvasNavbar" />
+      <Navbar.Offcanvas
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+        placement="end"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+              <NavDropdown.Item href="/">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/">Another action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/">Something else here</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex mt-3">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Offcanvas.Body>
+      </Navbar.Offcanvas>
+    </Navbar>
   );
 }
 
